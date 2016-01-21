710f9ba0c5344586947e55febd3ac75b3440703b
Fabian Köster <fabian.koester@bringnow.com>
2016-01-21 18:42:01 +0100

    More fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,25 @@
 touch /var/log/cron.log
 touch /etc/sync_env
 
-# Set git author info
-if [ -z ${GIT_USER_NAME} ]; then
+function die {
+    echo >&2 "$@"
+    exit 1
+}
+
+# Check mandatory environment variables
+if [ -z "${GIT_USER_NAME}" ]; then
   die "GIT_USER_NAME must be specified!"
 fi
-if [ -z ${GIT_USER_EMAIL} ]; then
+if [ -z "${GIT_USER_EMAIL}" ]; then
   die "GIT_USER_EMAIL must be specified!"
 fi
+if [ -z "${GIT_REPO_URL}" ]; then
+  die "GIT_REPO_URL must be specified!"
+fi
 
-# Set git author info and git behavior
+# Set git author info
 git config --global user.email "${GIT_USER_EMAIL}"
 git config --global user.name "${GIT_USER_NAME}"
-git config --global push.default simple
 
 if [ "$1" = "--overwrite-local"  ]; then
   echo OVERWRITE_LOCAL="true" >> /etc/sync_env
