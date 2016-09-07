c7f4d4e433ae60b65266e9ba3e67ee926ead3790
Fabian Köster <mail@fabian-koester.com>
2016-09-07 17:47:56 +0200

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,7 +39,7 @@ echo "GIT_REPO_URL=\"${GIT_REPO_URL}\"" >> /etc/sync_env
 # If not defined, the default is every minute
 CRON_TIME=${CRON_TIME:-*/1 * * * *}
 echo "Using cron time ${CRON_TIME}"
-echo "${CRONT_TIME} root /usr/local/bin/sync.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/git-sync
+echo "${CRON_TIME} root /usr/local/bin/sync.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/git-sync
 chmod 644 /etc/cron.d/git-sync
 
 if [ "$1" = "--overwrite-local"  ]; then
